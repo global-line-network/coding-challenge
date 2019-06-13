05e20dd7108ee34f3c770fc5835cf2a950fb58ab
Youssef Tharwat <info@yousseftharwat.me>
2019-06-13 16:00:11 +0800

    'seeder-fix'

--- app/User.php
+++ app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User
+class User extends Authenticatable
 {
     use Notifiable;
 
@@ -16,4 +18,24 @@ class User
     protected $fillable = [
         'name', 'date',
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    
 }
