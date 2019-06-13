c2e717ff6e9e0e45638e9c5777d073ce94cfdb92
Youssef Tharwat <info@yousseftharwat.me>
2019-06-13 17:46:26 +0800

    'fonts-fixed'

---
+++ resources/js/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="homeView">
+        <h2>Hey</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style lang="scss">
+    @import 'resources/sass/views/_home.scss';
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('')
+
+@section('content')
+    <home></home>
+@endsection
