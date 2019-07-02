76c4ac52fcecc8a4ff21df3ea5d2b7d685e56cf8
Jerome Rodrigo <jeromerodrigo@gmail.com>
2019-07-03 05:11:27 +0800

    Add createUser api call

--- gln-frontend/src/api/userApi.test.ts
+++ gln-frontend/src/api/userApi.test.ts
@@ -8,3 +8,16 @@ test("Can get users from api", async () => {
     expect(users).toBeDefined();
     expect(users.length).toBeGreaterThan(1);
 });
+
+test("Can create user on api", async () => {
+
+    const response = await UserApi.createUser({
+        id: -1,
+        name: "Hello",
+        birthDate: "test",
+        avatarUrl: ""
+    });
+
+    expect(response).toBeDefined();
+    expect(response.ok).toBe(true);
+});
--- gln-frontend/src/api/userApi.ts
+++ gln-frontend/src/api/userApi.ts
@@ -4,22 +4,39 @@ const ENDPOINT = "https://reqres.in";
 
 class UserApi {
 
-    static getUsers() : Promise<UserAccount[]> {
+    static async createUser(user: UserAccount) : Promise<Response> {
 
-        return fetch(`${ENDPOINT}/api/users?per_page=8`)
-            .then(res => {
-                return res.json();
-            })
-            .then(json => json.data.map((item : any) => {
-                const user : UserAccount = {
-                    id: item.id,
-                    name: `${item.first_name} ${item.last_name}`,
-                    avatarUrl: item.avatar,
-                    birthDate: "test"
-                };
+        const names = user.name.split(' ');
 
-                return user;
-            }));
+        return await fetch(`${ENDPOINT}/api/users`, {
+            method: 'POST',
+            body: JSON.stringify({
+                first_name: names[0],
+                last_name: names.length > 1 ? names[1] : '',
+                avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    static async getUsers() : Promise<UserAccount[]> {
+
+        const response = await fetch(`${ENDPOINT}/api/users?per_page=8`);
+
+        const responseJson = await response.json();
+
+        return responseJson.data.map((item : any) => {
+            const user : UserAccount = {
+                id: item.id,
+                name: `${item.first_name} ${item.last_name}`,
+                avatarUrl: item.avatar,
+                birthDate: "test"
+            };
+
+            return user;
+        });
     }
 }
 
