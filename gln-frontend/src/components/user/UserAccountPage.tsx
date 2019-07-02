b3a63ee57419ebaac3149434543f6ff6a9418912
Jerome Rodrigo <jeromerodrigo@gmail.com>
2019-07-03 04:38:24 +0800

    Add list item into main page

--- gln-frontend/src/components/user/UserAccountPage.tsx
+++ gln-frontend/src/components/user/UserAccountPage.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { UserAccount } from '../../api/types';
 import UserApi from '../../api/userApi';
+import UserAccountListItem from './UserAccountListItem';
 
 interface IUserAccountPageState {
     users: UserAccount[]
@@ -13,6 +14,25 @@ class UserAccountPage extends Component<any, IUserAccountPageState> {
         this.state = {
             users: []
         };
+
+        this.onEditConfirm = this.onEditConfirm.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+    }
+
+    onEditConfirm(editedUser: UserAccount) {
+        console.log("edit confirm!");
+
+        this.setState({
+            users: [
+                ...this.state.users.slice(0, editedUser.id - 1), 
+                editedUser,
+                ...this.state.users.slice(editedUser.id, this.state.users.length)
+            ]
+        });
+    }
+
+    onDelete(userId : number) {
+        console.log(`delete user ${userId} clicked!`);
     }
 
     componentDidMount() {
@@ -34,7 +54,7 @@ class UserAccountPage extends Component<any, IUserAccountPageState> {
     
                 <button type="button" className="btn btn-primary">+ Create New</button>
 
-                <code>{JSON.stringify(this.state.users)}</code>
+                {this.state.users && this.state.users.map(user => <UserAccountListItem key={user.id} user={user} onEdit={this.onEditConfirm} onDelete={this.onDelete} />)}
             </div>
         );
     }
