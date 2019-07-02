ecc4ab2f95b2073edcf661620444839de3bfec82
Jerome Rodrigo <jeromerodrigo@gmail.com>
2019-07-03 01:37:31 +0800

    Add UserAccountPage

---
+++ gln-frontend/src/components/user/UserAccountPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserAccountPage : React.FC = (props: any) => {
+
+    return (
+        <div>
+            <title>User Accounts</title>
+
+            <button type="button" className="btn btn-primary">+ Create New</button>
+        </div>
+    );
+};
+
+export default UserAccountPage;
