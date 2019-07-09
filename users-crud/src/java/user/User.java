e53af753f29956cbb8ab8cb8b2bb8bb80b7adbe4
Mohammad <mohammadyasrebi@gmail.com>
2019-07-09 21:38:48 +0430

    change the project structure and create the backend restfull apis

---
+++ users-crud/src/java/user/User.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Mohammad
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String date;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }    
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }    
+
+    /**
+     * Get the value of date
+     *
+     * @return the value of date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * Set the value of date
+     *
+     * @param date new value of date
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }    
+
+    /**
+     * Get the value of image
+     *
+     * @return the value of image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @param image new value of image
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", date=" + date + ", image=" + image + '}';
+    }
+
+}
