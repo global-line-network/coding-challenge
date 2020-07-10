1da8cc6820d1d191d1cfdad2c2269c8c543b3a28
Chris Ng <chris.ng860624@gmail.com>
2020-07-10 19:34:03 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+
+<script>
+import Users from "./components/Users";
+
+export default {
+  name: "App",
+  components: {
+
+  },
+  data() {}
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+#app {
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "USers"
+};
+</script>
