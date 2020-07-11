781d3646552284658871dcd25e4a8fc757930910
Chris Ng <chris.ng860624@gmail.com>
2020-07-11 15:35:08 +0800

    add: NavBar component

---
+++ src/components/NavBar/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <div class="nav-brand">
+      <img src="../../assets/logo.png" alt="Global Line Network" />
+    </div>
+  </nav>
+</template>
+
+<script>
+import "./style.scss";
+
+export default {
+  name: "NavBar"
+};
+</script>
