392610b7335de297c3fe910ac53ebf3a12fd9247
Chris Ng <chris.ng860624@gmail.com>
2020-07-11 15:29:40 +0800

    add: Main screen

---
+++ src/screens/Main/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-main">
+    <NavBar />
+    <h1 class="mt-5">{{pageTitle}}</h1>
+    <Users />
+  </div>
+</template>
+
+<script>
+import "./style.scss";
+
+import NavBar from "../../components/NavBar/index";
+import Users from "../../components/Users/index";
+
+export default {
+  name: "Main",
+  data: () => {
+    return {
+      pageTitle: "User Accounts"
+    };
+  },
+  components: {
+    NavBar,
+    Users
+  }
+};
+</script>
