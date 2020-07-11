76fe8da64a3d6e28d022ebfb0220a8bffae64e59
Chris Ng <chris.ng860624@gmail.com>
2020-07-11 15:36:03 +0800

    add: Users component

---
+++ src/components/Users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-users">
+    <button class="btn btn-primary btn-user-create">
+      <i class="fa fa-plus" aria-hidden="true"></i>
+      <span>Create User</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { fetchUsers } from "../../api/users";
+import "./style.scss";
+
+export default {
+  name: "Users",
+  methods: {
+    getUsers: async () => {
+      let users = await fetchUsers();
+      console.log(users);
+    }
+  }
+};
+</script>
