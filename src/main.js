0684d2e3ff28076b857e06b0d1288857caef5d2b
Chris Ng <chris.ng860624@gmail.com>
2020-07-12 12:19:38 +0800

    import fontawesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-// fontawesome
-import "@fortawesome/fontawesome-free/css/all.css";
-import '@fortawesome/fontawesome-free/js/all'
+// -- fontawesome --
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faSpinner } from '@fortawesome/free-regular-svg-icons';
+import {
+  faTrash,
+  faPencilAlt,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrash, faPencilAlt, faPlus);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+// !-- fontawesome --!
 
 Vue.config.productionTip = false;
 
