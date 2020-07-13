e24d9a1c414ab9552994d11c9ab6e2a6921afc44
Chris Ng <chris.ng860624@gmail.com>
2020-07-13 19:47:16 +0800

    add: UserForm screen

---
+++ src/components/Users/UserForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-user-form">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text">First Name</span>
+      </div>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        class="form-control"
+        aria-label="First name"
+        maxlength="16"
+      />
+
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Last Name</span>
+        </div>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          class="form-control"
+          aria-label="Last name"
+          maxlength="16"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserForm"
+};
+</script>
