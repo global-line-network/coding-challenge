d853559fcd6bd9ff6d87e7728d1a511c907ae832
Chris Ng <chris.ng860624@gmail.com>
2020-07-13 19:46:45 +0800

    add: Modal screen

---
+++ src/components/Modal/index.vue
@@ -0,0 +1,30 @@
+<template>
+<modal class="container-modal" name="vue-modal">
+  <header>
+    <button @click="hide" class="btn btn-sm btn-danger">
+      <font-awesome-icon :icon="['fas', 'times']" />
+    </button>
+
+    <slot name="modal-header" />
+  </header>
+  <body>
+    <slot name="modal-body" />
+  </body>
+  <footer>
+    <slot name="modal-footer" />
+  </footer>
+</modal>
+</template>
+
+<script>
+import "./style.scss";
+
+export default {
+  name: "ModalContent",
+  methods: {
+    hide() {
+      this.$modal.hide("vue-modal");
+    }
+  }
+};
+</script>
