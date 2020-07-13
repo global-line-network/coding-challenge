997311be5f6eb4e24c412ec22d85266655b069ad
Chris Ng <chris.ng860624@gmail.com>
2020-07-13 19:45:50 +0800

    import: fas icon

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import VueLazyLoad from "vue-lazyload";
+import VModal from "vue-js-modal";
 
 // -- fontawesome --
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -10,15 +11,20 @@ import {
   faTrash,
   faPencilAlt,
   faPlus,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faTrash, faPencilAlt, faPlus);
+library.add(faTrash, faPencilAlt, faPlus, faTimes);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 // !-- fontawesome --!
 
 // -- lazyload --
 Vue.use(VueLazyLoad);
 // !-- lazyload --!
+
+// -- modal --
+Vue.use(VModal);
+// !-- modal --!
 
 Vue.config.productionTip = false;
 
