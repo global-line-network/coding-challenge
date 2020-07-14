8f8e609fa90dd4021f33517401f7b4870c8f60ef
Chris Ng <chris.ng860624@gmail.com>
2020-07-14 23:18:15 +0800

    modify: move modal into container-user

--- src/components/Users/UserCard.vue
+++ src/components/Users/UserCard.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="container-user-card row"
-    v-lazy-container="{selector: 'img', loading: 'https://www.jettools.com/images/animated_spinner.gif'}"
-  >
+  <div class="container-users">
     <!-- modal -->
     <ModalContent>
       <template v-slot:modal-header>
@@ -11,61 +8,76 @@
       <template v-slot:modal-body>
         <UserForm />
       </template>
-      <template v-slot:modal-footer></template>
+      <template v-slot:modal-footer>
+        <div class="modal-actions">
+          <button type="submit" class="btn btn-success d-flex ml-auto">Create</button>
+        </div>
+      </template>
     </ModalContent>
     <!-- !--modal--! -->
 
+    <!-- modal buttons -->
     <div class="container-user-create">
       <button @click="show" class="btn btn-primary btn-user-create">
         <font-awesome-icon :icon="['fas', 'plus']" />
         <span>Create User</span>
       </button>
     </div>
+    <!-- !--modal buttons--! -->
+    <div
+      class="container-user-card row"
+      v-lazy-container="{selector: 'img', loading: 'https://www.jettools.com/images/animated_spinner.gif'}"
+    >
+      <div
+        v-for="user in userList"
+        :key="user.id"
+        :data-id="user.id"
+        class="card col-xs-8 col-sm-6 col-md-2"
+      >
+        <div class="card-body">
+          <div class="card-user-img">
+            <img
+              class="user-card-img-top"
+              v-bind:data-src="user.avatar"
+              v-bind:alt="`${user.first_name} ${user.last_name}`"
+            />
+          </div>
+          <div class="card-user-info">
+            <input
+              disabled
+              placeholder="Name"
+              type="text"
+              class="card-title user-info-name"
+              maxlength="16"
+              :value="`${user.first_name} ${user.last_name}`"
+            />
 
-    <div v-for="user in userList" :key="user.id" class="card col-xs-8 col-sm-6 col-md-2">
-      <div class="card-body">
-        <div class="card-user-img">
-          <img
-            class="user-card-img-top"
-            v-bind:data-src="user.avatar"
-            v-bind:alt="`${user.first_name} ${user.last_name}`"
-          />
-        </div>
-        <div class="card-user-info">
-          <input
-            disabled
-            placeholder="Name"
-            type="text"
-            class="card-title user-info-name"
-            maxlength="16"
-            :value="user.first_name + ' ' +  user.last_name"
-          />
-
-          <input
-            disabled
-            placeholder="Registered Date"
-            type="text"
-            class="card-title user-info-date"
-            maxlength="16"
-            :value="'12/14/2019'"
-          />
-        </div>
-        <div class="card-user-actions">
-          <button class="btn btn-default btn-user-edit">
-            <font-awesome-icon :icon="['fas', 'pencil-alt']" />
-          </button>
-          <button class="btn btn-default circle btn-user-edit">
-            <font-awesome-icon :icon="['fas', 'trash']" />
-          </button>
+            <input
+              disabled
+              placeholder="Registered Date"
+              type="text"
+              class="card-title user-info-date"
+              maxlength="16"
+              :value="'12/14/2019'"
+            />
+          </div>
+          <div class="card-user-actions">
+            <button class="btn btn-default btn-user-edit">
+              <font-awesome-icon :icon="['fas', 'pencil-alt']" />
+            </button>
+            <button @click="removeUser" class="btn btn-default circle btn-user-remove">
+              <font-awesome-icon :icon="['fas', 'trash']" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <i aria-hidden="true"></i>
-    <i aria-hidden="true"></i>
-    <i aria-hidden="true"></i>
-    <i aria-hidden="true"></i>
-    <i aria-hidden="true"></i>
+      <i aria-hidden="true"></i>
+      <i aria-hidden="true"></i>
+      <i aria-hidden="true"></i>
+      <i aria-hidden="true"></i>
+      <i aria-hidden="true"></i>
+    </div>
   </div>
 </template>
 
@@ -73,6 +85,11 @@
 import "./style.scss";
 import UserForm from "./UserForm";
 import ModalContent from "../Modal/index";
+
+import { user } from "../../store";
+
+// import { createUser, deleteUser } from "../../api/users";
+import { deleteUser } from "../../api/users";
 
 export default {
   name: "UserCard",
@@ -84,6 +101,23 @@ export default {
   methods: {
     show() {
       this.$modal.show("vue-modal");
+    },
+    hide() {
+      this.$modal.hide("vue-modal");
+    },
+    removeUser(e) {
+      let username = e.target.closest(".card").querySelector(".user-info-name")
+        .value;
+
+      let userID = e.target.closest(".card").dataset.id;
+
+      this.$confirm(`Confirm to delete user [${username}] ?`).then(() => {
+        // api callback
+        deleteUser();
+
+        // update state from store
+        user.list = user.list.filter(user => user.id != userID);
+      });
     }
   }
 };
