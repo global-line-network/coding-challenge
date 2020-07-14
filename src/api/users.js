ce31768d79d33f2c2a1aa7a182f43f776feace53
Chris Ng <chris.ng860624@gmail.com>
2020-07-14 23:31:21 +0800

    modify: api uri

--- src/api/users.js
+++ src/api/users.js
@@ -2,7 +2,7 @@ import Constants from "../constants";
 import axios from "axios";
 
 export function fetchUsers() {
-  let url = Constants.users.fetch;
+  let url = `${Constants.users.fetch}/users?page=2`;
   return new Promise((resolve, reject) => {
     axios
       .get(url, {
@@ -20,3 +20,30 @@ export function fetchUsers() {
       });
   });
 }
+
+export function createUser(formData) {
+  let url = `${Constants.users.fetch}/users`;
+  let { first_name, last_name } = formData;
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        user: {
+          first_name,
+          last_name,
+        },
+      })
+      .then((res) => {
+        let data = res.data;
+        resolve(data).then((res) => res.json());
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export function deleteUser() {}
