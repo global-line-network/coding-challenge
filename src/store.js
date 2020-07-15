f5aa4803b1b54c728455bab2e906fd217d9732ee
Chris Ng <chris.ng860624@gmail.com>
2020-07-15 16:33:09 +0800

    add: spiner object state

--- src/store.js
+++ src/store.js
@@ -3,3 +3,10 @@ import Vue from "vue";
 export const user = Vue.observable({
   list: []
 });
+
+export const spinner = Vue.observable({
+  createUserBtn: {
+    text: 'Create',
+    spinner: 'fade'
+  }
+})
