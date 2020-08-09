1b31fd695fed809245563acc15da2956a8033738
Shuwair Sardar <shuwair@gmail.com>
2020-08-09 22:37:58 +0800

    Added test cases

--- src/main/java/com/gln/entity/User.java
+++ src/main/java/com/gln/entity/User.java
@@ -46,4 +46,20 @@ public class User {
     public String getAvatar() {
         return avatar;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 }
--- src/test/java/com/gln/UserTest.java
+++ src/test/java/com/gln/UserTest.java
@@ -3,14 +3,17 @@ package com.gln;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gln.entity.User;
 import com.gln.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -24,15 +27,66 @@ public class UserTest {
     @Autowired
     private ObjectMapper mapper;
 
+    private User user;
+    private String userJson;
+
+    @BeforeEach
+    public void loadUser() throws Exception {
+        user = new User("first", "last", "abc@emil.com", "www.avatar.com");
+        userJson = mapper.writeValueAsString(user);
+    }
+
+
     @Test
     public void createUserTest() throws Exception {
 
-        User user = new User("John", "Cena", "abc@emil.com", "www.avatar.com");
-        String json = mapper.writeValueAsString(user);
+        this.mockMvc.perform(post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateUserTest() throws Exception {
 
         this.mockMvc.perform(post("/user/create")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(json)
+                .content(userJson)
         ).andExpect(status().isOk());
+
+        MvcResult result = this.mockMvc.perform(get("/user/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson)).andReturn();
+
+        User updateUser = mapper.readValue(result.getResponse().getContentAsString(), User.class);
+        updateUser.setFirstName("ufirstName");
+        updateUser.setLastName("ulastName");
+
+
+        this.mockMvc.perform(put("/user/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateUser))
+        ).andExpect(status().isOk()).andExpect(content().json(mapper.writeValueAsString(updateUser)));
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+
+        this.mockMvc.perform(post("/user/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson)
+        ).andExpect(status().isOk());
+
+        MvcResult result = this.mockMvc.perform(get("/user/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson)).andReturn();
+
+        User deleteUser = mapper.readValue(result.getResponse().getContentAsString(), User.class);
+
+
+        this.mockMvc.perform(delete("/user/delete")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(deleteUser))
+        ).andExpect(status().isAccepted());
     }
 }
