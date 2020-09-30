c2a28467da94aa5528268e26d8dd8a1bb18465cc
rully <mohd.rully.k@gmail.com>
2020-09-30 17:31:45 +0700

    Add auth testing method

--- src/test/java/com/gln/resource/UserAccountResourceTest.java
+++ src/test/java/com/gln/resource/UserAccountResourceTest.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @QuarkusTest
 public class UserAccountResourceTest {
@@ -20,8 +22,18 @@ public class UserAccountResourceTest {
     UserAccountRepository mockRepository;
 
     @Test
+    void getUserIdWithoutAuthenticationTest() {
+        long userAccountID  = 2;
+        given()
+                .when().get("/api/users/"+userAccountID)
+                .then()
+                .statusCode(401);
+        verify(mockRepository, times(0)).findByIdOptional(userAccountID);
+    }
+
+    @Test
     @TestSecurity(authorizationEnabled = false)
-    void getUserById() {
+    void getUserByIdTest() {
         long userAccountID  = 2;
         Mockito.when(mockRepository.findByIdOptional(userAccountID)).thenReturn(createSampleUser());
 
@@ -31,6 +43,7 @@ public class UserAccountResourceTest {
                 .statusCode(200)
                 .body("ad.company", is("StatusCode Weekly"))
                 .body("data.name", is("Ramsey"));
+        verify(mockRepository, times(1)).findByIdOptional(userAccountID);
     }
 
     private static Optional<UserAccount> createSampleUser(){
