1d9d9dc1c19ae77ddc44e690481d4da60c8f10f8
azwar <azwar.akbar@gmail.com>
2020-10-01 10:44:03 +0700

    Create class entity BaseModel and User

---
+++ src/main/java/com/example/azwarakbar/models/User.java
@@ -0,0 +1,59 @@
+package com.example.azwarakbar.models;
+
+import io.ebean.annotation.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User extends BaseModel {
+
+    @NotNull
+    String email;
+    String firstName;
+    String lastName;
+    String avatar;
+    @NotNull
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
