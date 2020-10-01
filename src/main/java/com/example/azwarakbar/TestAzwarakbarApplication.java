acfc1d6323b85bcda74835efb54d4727e448f525
azwar <azwar.akbar@gmail.com>
2020-10-01 09:58:22 +0700

    Add project skeleton, initial commit.

---
+++ src/main/java/com/example/azwarakbar/TestAzwarakbarApplication.java
@@ -0,0 +1,13 @@
+package com.example.azwarakbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAzwarakbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAzwarakbarApplication.class, args);
+	}
+
+}
