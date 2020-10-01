da5c0d1bcdc7d7a0f3070ddbf8e4a56d229192a5
azwar <azwar.akbar@gmail.com>
2020-10-01 13:51:56 +0700

    Done: login function

--- src/main/java/com/example/azwarakbar/controller/UserController.java
+++ src/main/java/com/example/azwarakbar/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.azwarakbar.controller;
 
 import com.example.azwarakbar.models.User;
+import com.example.azwarakbar.request.LoginUser;
 import com.example.azwarakbar.response.RegistrationSuccess;
 import com.example.azwarakbar.services.UserService;
 import com.example.azwarakbar.utils.ResponseUtils;
 import com.example.azwarakbar.utils.ZJson;
 import com.example.azwarakbar.utils.ZString;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,5 +57,43 @@ public class UserController {
         ObjectNode objectNode = ZJson.toObjectNode(rs);
 
         return ResponseEntity.ok(objectNode);
+    }
+
+    @PostMapping(value = "/api/login", consumes = "application/json")
+    @ResponseBody
+    public ResponseEntity<ObjectNode> login(@RequestBody LoginUser user) {
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Set<ConstraintViolation<LoginUser>> constraintViolations = validator.validate(user);
+        List<String> listError = new ArrayList<>();
+
+        if (constraintViolations.size() > 0 ) {
+
+            for (ConstraintViolation<LoginUser> contraints : constraintViolations) {
+                String strError = contraints.getPropertyPath() + ": " + contraints.getMessage();
+                listError.add(strError);
+            }
+
+            ArrayNode arrayNode = ZJson.toArrayNode(listError);
+
+            return new ResponseEntity<>(ResponseUtils.getError(arrayNode), HttpStatus.BAD_REQUEST);
+        }
+
+        boolean isLoginOk = service.check(user);
+
+        if (! isLoginOk) {
+            ObjectNode objNode = ResponseUtils.getError("Wrong email or password");
+            return new ResponseEntity<>(objNode, HttpStatus.BAD_REQUEST);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objNode = mapper.createObjectNode();
+
+        String token = ZString.generateRandomStr();
+        objNode.put("token", token);
+
+        return ResponseEntity.ok(objNode);
     }
 }
