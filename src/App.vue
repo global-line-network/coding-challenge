2446028b9ad3d05cca081dc9e50b63e3936eba70
mokwangquan <mokwangquan@gmail.com>
2021-04-26 20:46:13 +0800

    First commit with skeleton app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Caveat:wght@600&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@300&display=swap"
+      rel="stylesheet"
+    />
+    <router-view :key="$route.fullPath" />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
